7d53ff62da035f5faca8a8089ebfb1740d6764d2
Hanna_Urban <huan92@yandex.ru>
2024-01-13 01:33:32 +0300

    tasks are implemented

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.module3.task4;
-
+import java.util.Scanner;
 public class FormulaCreator {
     public static void main(String[] args) {
+        Scanner user_input = new Scanner (System.in);
+        float a;
+        a = user_input.nextFloat();
+        float b;
+        b = user_input.nextFloat();
+        float firstBrackets;
+        firstBrackets=9*a*a-5*b+2+a-7;
+        float secondBracketsDividend;
+        byte secondBracketsDivisor;
+        secondBracketsDividend=a+b-4*a*b;
+        secondBracketsDivisor=2;
+        float secondBrackets;
+        secondBrackets=secondBracketsDividend/secondBracketsDivisor;
+        float result;
+        result=firstBrackets*secondBrackets;
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int firstLetter = 99;
+        char a = (char) firstLetter;
+        int secondLetter = 104;
+        char b = (char) secondLetter;
+        int thirdLetter = 97;
+        char c = (char) thirdLetter;
+        int fourthLetter = 114;
+        char d = (char) fourthLetter;
+        System.out.println(a+""+b+""+c+""+d);
     }
 }
+//Print chars :'c', 'h', 'a', 'r' using int representation.
