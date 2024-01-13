94c59ddb8a7c514bdd4df4b5e2d7a5c30775cd24
Hanna_Urban <huan92@yandex.ru>
2024-01-14 00:09:13 +0300

    tasks are implemented

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,12 +1,11 @@
 package school.mjc.stage0.module3.task4;
-import java.util.Scanner;
+
 public class FormulaCreator {
     public static void main(String[] args) {
-      Scanner user_input = new Scanner (System.in);
         float a;
-        a = user_input.nextFloat();
+        a = 1;
         float b;
-       b = user_input.nextFloat();
+       b = 2;
         float result;
         result=(9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
         System.out.println(result);
